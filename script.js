f905c0242a75826c6defa532044f8e0aa45a0bf0
Trinh Doan <trinhd997@gmail.com>
2026-01-16 11:18:45 +0200

    calculate mortgage logic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const form = document.getElementById("form");
+
 const amountSymbol = document.getElementById("amount-symbol");
 const termSymbol = document.getElementById("term-symbol");
 const interestSymbol = document.getElementById("interest-symbol");
@@ -18,8 +20,8 @@ const calculateBtn = document.getElementById("calculate-btn");
 //output
 const emptyResult = document.getElementById("empty-result");
 const calculatedResult = document.getElementById("calculated-result");
-const monthlyRepayment = document.getElementById("monthly-repayments");
-const totalRepayment = document.getElementById("total-repayments");
+const monthlyRepayment = document.getElementById("monthly-repay");
+const totalRepayment = document.getElementById("total-repay");
 
 function updateErrorClass(element, isError, errorClass) {
     if (element.classList.contains(errorClass)) {
@@ -40,21 +42,99 @@ function handleRadioBtn(event) {
     event.target.parentElement.classList.add("radio-selected"); //hard-coded way to access the div container
 }
 
-function handleCalculateBtn() {
-    updateErrorClass(amountSymbol, true, "error-symbol");
-    updateErrorClass(mortgageAmount.parentElement, true, "error-input");
-    amountError.style.display = "block";
+function validateAndCalculateMortgage() {
+    const validation = {
+        "amount": false,
+        "term": false,
+        "interest": false,
+    };
 
-    updateErrorClass(termSymbol, true, "error-symbol");
-    updateErrorClass(mortgageTerm.parentElement, true, "error-input");
-    termError.style.display = "block";
+    const amountStr = mortgageAmount.value.trim();
+    const termStr = mortgageTerm.value.trim();
+    const interestStr = mortgageInterest.value.trim();
+    let amount = 0, term = 0, interest = 0;
 
-    updateErrorClass(interestSymbol, true, "error-symbol");
-    updateErrorClass(mortgageInterest.parentElement, true, "error-input");
-    interestError.style.display = "block";
-    typeError.style.display = "block";
+    if (amountStr) {
+        amount = Number(amountStr);
+        validation["amount"] = true;
+    }
+    else {
+        updateErrorClass(amountSymbol, true, "error-symbol");
+        updateErrorClass(mortgageAmount.parentElement, true, "error-input");
+        amountError.style.display = "block";
+    }
+
+    if (termStr) {
+        term = Number(termStr);
+        validation["term"] = true;
+    }
+    else {
+        updateErrorClass(termSymbol, true, "error-symbol");
+        updateErrorClass(mortgageTerm.parentElement, true, "error-input");
+        termError.style.display = "block";
+    }
+
+
+    if (interestStr) {
+        interest = Number(interestStr);
+        validation["interest"] = true;
+    }
+    else {
+        updateErrorClass(interestSymbol, true, "error-symbol");
+        updateErrorClass(mortgageInterest.parentElement, true, "error-input");
+        interestError.style.display = "block";
+    }
+
+    if (repaymentType.checked || interestType.checked) {
+        if (validation["amount"] && validation["term"] && validation["interest"]) {
+            calculateMortgage(amount, term, interest, interestType.checked);
+        }
+    }
+    else {
+        typeError.style.display = "block";
+    }
+}
+
+function resetOutput() {
+    updateErrorClass(amountSymbol, false, "error-symbol");
+    updateErrorClass(mortgageAmount.parentElement, false, "error-input");
+    amountError.style.display = "none";
+
+    updateErrorClass(termSymbol, false, "error-symbol");
+    updateErrorClass(mortgageTerm.parentElement, false, "error-input");
+    termError.style.display = "none";
+
+    updateErrorClass(interestSymbol, false, "error-symbol");
+    updateErrorClass(mortgageInterest.parentElement, false, "error-input");
+    interestError.style.display = "none";
+    typeError.style.display = "none";
+    emptyResult.style.display = "flex";
+    calculatedResult.style.display = "none";
+}
+
+function calculateMortgage(amount, term, interest, isInterestOnly) {
+    emptyResult.style.display = "none";
+    calculatedResult.style.display = "flex";
+    const monthlyInterst = interest / (12 * 100);
+    const totalMonths = term * 12;
+    const powerVar = Math.pow((monthlyInterst + 1), totalMonths);
+    const payPerMonth = amount * monthlyInterst * powerVar / (powerVar - 1);
+    const totalAmount = payPerMonth * totalMonths;
+    if (isInterestOnly) {
+        const interestAmount = totalAmount - amount;
+        monthlyRepayment.textContent = `${(interestAmount / totalMonths).toFixed(2)}`;
+        totalRepayment.textContent = `${interestAmount.toFixed(2)}`;
+    }
+    else {
+        monthlyRepayment.textContent = `${payPerMonth.toFixed(2)}`;
+        totalRepayment.textContent = `${totalAmount.toFixed(2)}`;
+    }
 }
 
 repaymentType.addEventListener("change", handleRadioBtn);
 interestType.addEventListener("change", handleRadioBtn);
-calculateBtn.addEventListener("click", () => { handleCalculateBtn(); });
+form.addEventListener("submit", (event) => {
+    resetOutput();
+    event.preventDefault();
+    validateAndCalculateMortgage();
+});
